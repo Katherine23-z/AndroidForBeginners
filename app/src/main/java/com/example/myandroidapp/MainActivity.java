8792379f70587f9e3de8e9b905c9b2c840f1d869
Katherine23-z <eszaharova23@yandex.ru>
2021-05-09 15:23:04 +0300

    Add math logic

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -3,21 +3,38 @@ package com.example.myandroidapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Locale;
+
+import javax.crypto.spec.OAEPParameterSpec;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    boolean plus;
+    boolean minus;
+    boolean multiply;
+    boolean split;
+    boolean percent;
+
     private TextView textView;
+    private EditText editText;
+
+    private double valueOne;
+    private double valueTwo;
+    private double result;
+
     String numberField;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,33 +42,90 @@ public class MainActivity extends AppCompatActivity {
 
         String instanceState;
 
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             instanceState = "Первый запуск";
-        }else{
+        } else {
             instanceState = "Повторный запуск";
         }
 
-        Log.d(TAG, "onCreate " + instanceState );
+        Log.d(TAG, "onCreate " + instanceState);
 
-        textView = findViewById(R.id.editText);
-        findViewById(R.id.button1).setOnClickListener(v -> textView.setText(String.format("%s1", textView.getText())));
-        findViewById(R.id.button2).setOnClickListener(v -> textView.setText(String.format("%s2", textView.getText())));
-        findViewById(R.id.button3).setOnClickListener(v -> textView.setText(String.format("%s3", textView.getText())));
-        findViewById(R.id.button4).setOnClickListener(v -> textView.setText(String.format("%s4", textView.getText())));
-        findViewById(R.id.button5).setOnClickListener(v -> textView.setText(String.format("%s5", textView.getText())));
-        findViewById(R.id.button6).setOnClickListener(v -> textView.setText(String.format("%s6", textView.getText())));
-        findViewById(R.id.button7).setOnClickListener(v -> textView.setText(String.format("%s7", textView.getText())));
-        findViewById(R.id.button8).setOnClickListener(v -> textView.setText(String.format("%s8", textView.getText())));
-        findViewById(R.id.button9).setOnClickListener(v -> textView.setText(String.format("%s9", textView.getText())));
-        findViewById(R.id.button0).setOnClickListener(v -> textView.setText(String.format("%s0", textView.getText())));
-        findViewById(R.id.button_plus).setOnClickListener(v -> textView.setText(String.format("%s+", textView.getText())));
-        findViewById(R.id.button_minus).setOnClickListener(v -> textView.setText(String.format("%s-", textView.getText())));
-        findViewById(R.id.button_multiply).setOnClickListener(v -> textView.setText(String.format("%s*", textView.getText())));
-        findViewById(R.id.button_split).setOnClickListener(v -> textView.setText(String.format("%s/", textView.getText())));
-        findViewById(R.id.button_equal).setOnClickListener(v -> textView.setText(String.format("%s=", textView.getText())));
-        findViewById(R.id.button_percent).setOnClickListener(v -> textView.setText(String.format("%s%%", textView.getText())));
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button1).setOnClickListener(v -> editText.setText(String.format("%s1", editText.getText())));
+        findViewById(R.id.button2).setOnClickListener(v -> editText.setText(String.format("%s2", editText.getText())));
+        findViewById(R.id.button3).setOnClickListener(v -> editText.setText(String.format("%s3", editText.getText())));
+        findViewById(R.id.button4).setOnClickListener(v -> editText.setText(String.format("%s4", editText.getText())));
+        findViewById(R.id.button5).setOnClickListener(v -> editText.setText(String.format("%s5", editText.getText())));
+        findViewById(R.id.button6).setOnClickListener(v -> editText.setText(String.format("%s6", editText.getText())));
+        findViewById(R.id.button7).setOnClickListener(v -> editText.setText(String.format("%s7", editText.getText())));
+        findViewById(R.id.button8).setOnClickListener(v -> editText.setText(String.format("%s8", editText.getText())));
+        findViewById(R.id.button9).setOnClickListener(v -> editText.setText(String.format("%s9", editText.getText())));
+        findViewById(R.id.button0).setOnClickListener(v -> editText.setText(String.format("%s0", editText.getText())));
+        findViewById(R.id.button_dot).setOnClickListener(v -> editText.setText(String.format("%s.", (editText.getText()))));
+        findViewById(R.id.button_plus).setOnClickListener(v -> {
+            valueOne = Double.parseDouble(editText.getText().toString());
+            plus = true;
+            textView.setText(valueOne + "+");
+            editText.setText(null);
+        });
 
+        findViewById(R.id.button_minus).setOnClickListener(v -> {
+            valueOne = Double.parseDouble(editText.getText().toString());
+            minus = true;
+            textView.setText(valueOne + "-");
+            editText.setText(null);
+        });
 
+        findViewById(R.id.button_multiply).setOnClickListener(v -> {
+            valueOne = Double.parseDouble(editText.getText().toString());
+            multiply = true;
+            textView.setText(valueOne + "*");
+            editText.setText(null);
+        });
+
+        findViewById(R.id.button_split).setOnClickListener(v -> {
+            valueOne = Double.parseDouble(editText.getText().toString());
+            split = true;
+            textView.setText(valueOne + "/");
+            editText.setText(null);
+        });
+
+        findViewById(R.id.button_percent).setOnClickListener(v -> {
+            valueOne = Double.parseDouble(editText.getText().toString());
+            percent = true;
+            textView.setText(valueOne + "%");
+            editText.setText(null);
+        });
+
+        findViewById(R.id.button_clear).setOnClickListener(v -> editText.setText(null));
+
+        findViewById(R.id.button_equal).setOnClickListener(v -> {
+            calculate();
+            textView.setText(textView.getText().toString() + valueTwo + "=" + result);
+        });
+
+    }
+
+    private void calculate() {
+        valueTwo = Double.parseDouble(editText.getText().toString());
+        editText.setText(null);
+        if (plus){
+            result = valueOne + valueTwo;
+            plus = false;
+        }else if (minus){
+            result = valueOne - valueTwo;
+            minus = false;
+        }else if (multiply){
+            result = valueOne * valueTwo;
+            multiply = false;
+        }else if (split){
+            result = valueOne/valueTwo;
+            split = false;
+        }else if (percent){
+            result = valueOne * valueTwo / 100;
+            percent = false;
+        }
     }
 
     @Override
