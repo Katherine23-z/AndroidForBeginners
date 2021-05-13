2b38725e3bd5b2bb4344b9de1e0b4badf9b2ad2f
Katherine23-z <eszaharova23@yandex.ru>
2021-05-13 20:53:08 +0300

    Add Themes

---
+++ app/src/main/java/com/example/myandroidapp/Constants.java
@@ -0,0 +1,5 @@
+package com.example.myandroidapp;
+
+public interface Constants {
+    String SELECTED_THEME = "SELECTED_THEME";
+}
--- app/src/main/java/com/example/myandroidapp/MySecondActivity.java
+++ app/src/main/java/com/example/myandroidapp/MySecondActivity.java
@@ -2,6 +2,8 @@ package com.example.myandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 public class MySecondActivity extends AppCompatActivity {
@@ -12,5 +14,13 @@ public class MySecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_second);
 
         findViewById(R.id.button_return).setOnClickListener(v -> finish());
+        findViewById(R.id.button_night).setOnClickListener(v -> {
+            setTheme(R.style.Theme_MyAndroidApp_Dark);
+            recreate();
+        });
+        findViewById(R.id.button_no_night).setOnClickListener(v -> {
+            setTheme(R.style.Theme_MyAndroidApp_Light);
+            recreate();
+        });
     }
 }
